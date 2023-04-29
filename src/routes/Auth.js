2a22b3a7f57aa3c4c73e80a3f317142ecf998d3f
bookhabit <qnrgoqlt@naver.com>
2023-04-30 04:53:22 +0900

    회원가입 - create Account

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,12 @@
+import { getAuth,createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 
 const Auth = ()=>{
     const [email,setEmail] = useState("")
     const [password,setPassword] = useState("")
+    const [newAccount,setNewAccount] = useState(true)
+    const auth = getAuth();
+
     const onChange = (event)=>{
         const {target:{name,value}} = event;
         if(name=="email"){
@@ -11,16 +15,28 @@ const Auth = ()=>{
             setPassword(value)
         }
     }
-    console.log(email,password)
-    const onSubmit = (event)=>{
+    const onSubmit = async (event)=>{
         event.preventDefault();
+        try{
+            let data;
+            if(newAccount){
+                // create Account
+                data = await createUserWithEmailAndPassword(auth,email,password)
+            }else{
+                // log in
+                data = await signInWithEmailAndPassword(auth,email,password)
+            }
+            console.log('data',data)
+        }catch(error){
+            console.log(error)
+        }
     }
     return(
         <>
             <form onSubmit={onSubmit}>
                 <input type="text" name="email" placeholder="Email" required value={email} onChange={onChange}/>
                 <input type="password" name="password" placeholder="password" required value={password} onChange={onChange} />
-                <input type="submit" value="Log in"/>
+                <input type="submit" value={newAccount ? "Create Account" :"Log in"}/>
             </form>
             <div>
                 <button>Continue with Google</button>
