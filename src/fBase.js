5d0ccedce61ae5217cc9d175dba12078841aa03b
bookhabit <qnrgoqlt@naver.com>
2023-05-01 21:17:46 +0900

    게시글 생성

--- src/fBase.js
+++ src/fBase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
+import "firebase/database"
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,5 +16,5 @@ const firebaseConfig = {
   // Initialize Firebase
 const fbase = initializeApp(firebaseConfig);
 export const authService = getAuth();
-
+export const dbService = getFirestore();
 export default fbase
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,35 @@
-import React from "react";
+import React, { useState } from 'react';
+import { addDoc,collection } from 'firebase/firestore';
+import { dbService } from 'fBase';
 
-export default ()=><span>Home</span>
+const Home = () => {
+    const [nweet,setNweet] = useState("");
+    const onSubmit = async (event)=>{
+        event.preventDefault();
+        try{
+            const docRef = await addDoc(collection(dbService,"nweets"),{
+                nweet,
+                createdAt:Date.now()
+            })
+            console.log('docRef',docRef)
+            console.log('docRef.id',docRef.id)
+        }catch(error){
+            console.log(error)
+        }
+        setNweet("")
+    }
+    const onChangeNweet=(event)=>{
+        const {target:{value}} = event;
+        setNweet(value)
+    }
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input type='text' placeholder="What's on your mind?" maxLength={120} value={nweet} onChange={onChangeNweet}/>
+                <input type='submit' value="Nweet"/>
+            </form>
+        </div>
+    );
+};
+
+export default Home;
